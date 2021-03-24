c1c2b21dbe0cf6464887ceb1dcfcfeab9c19e742
ArchanaKalathil <archanakvenu@gmail.com>
2021-03-24 13:26:01 -0400

    adding people counter based on HOG

---
+++ counter_cpp/build/CMakeFiles/people_counter.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/people_counter.dir/people_counter.cpp.o"
+  "people_counter"
+  "people_counter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/people_counter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
